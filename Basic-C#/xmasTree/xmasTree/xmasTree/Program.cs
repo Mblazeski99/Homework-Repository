d1678a2a0760b4ccc33f302234e031d6aa9768ce
Marko Blazheski <markoblazeski1999@gmail.com>
2019-03-01 12:01:36 +0100

    Add files via upload
    Homework for class of Basic C#

---
+++ Basic-C#/xmasTree/xmasTree/xmasTree/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xmasTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            void makeXmasTree()
+            {
+                string stars = "*";
+                int emptySpaces = 10;
+                string tree = "";
+                for (int i = 0; i < 10; i++)
+                {
+                    tree = "";
+                    for (int j = emptySpaces; j > 0; j--)
+                    {
+                        tree += " ";
+                    }
+                    emptySpaces--;
+                    tree += stars;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(tree);
+                    stars += "**";
+                }
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("         ||");
+                Console.WriteLine("         ||");
+                Console.WriteLine("         ||");
+                Console.WriteLine("         ||");
+                Console.ReadKey();
+            }
+            makeXmasTree();
+        }
+    }
+}
