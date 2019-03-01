6d0b646d92f9a5146bf8a08130cc2bd05a3d4755
Marko Blazheski <markoblazeski1999@gmail.com>
2019-03-01 11:57:44 +0100

    Add files via upload

---
+++ Basic-C#/ChoosePrize/ChoosePrize/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChoosePrize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number from 1 to 3");
+            int.TryParse(Console.ReadLine(), out int InputNumber);
+            switch(InputNumber)
+            {
+                case 1:
+                    Console.WriteLine("You got a new car!");
+                    break;
+                case 2:
+                    Console.WriteLine("You got a new plane!");
+                    break;
+                case 3:
+                    Console.WriteLine("You got a new bike!");
+                    break;
+                default:
+                    Console.WriteLine("You get nothing good day sir!");
+                        break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
