8b0d4488d98cc071138790e88cf3cf81129a309a
Marko Blazheski <markoblazeski1999@gmail.com>
2019-03-08 16:08:56 +0100

    Add files via upload

---
+++ Basic-C#/arraySum/arraySum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] sumArray = new int[5];
+            for(int i = 0; i<=4; i++)
+            {
+                Console.WriteLine($"Enter number {i + 1}");
+                if(int.TryParse(Console.ReadLine(), out sumArray[i]))
+                {
+                    sum += sumArray[i];
+                } else
+                {
+                    Console.WriteLine("you did not enter a number");
+                    break;
+                }
+                
+            }
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"the sum of all numbers is {sum}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
